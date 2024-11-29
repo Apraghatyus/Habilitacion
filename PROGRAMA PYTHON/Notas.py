2684d4500fdb9022b93b90d691ddec09a4cad8db
Apraghatyus <cardonarealpe@gmail.com>
2024-11-28 22:38:13 -0500

    Implementar bucle para agregar los estudiantes

--- PROGRAMA PYTHON/Notas.py
+++ PROGRAMA PYTHON/Notas.py
@@ -18,34 +18,44 @@ Adicionalmente, se está interasado en analizar los datos de los estudiantes jun
  '''
 #Esta será la funcion que calcule los promedios
 def Promedio(nota1, nota2, nota3):
-    promedio = (nota1 + nota2 + nota3) / 3
-    return promedio
+    prom = (nota1 + nota2 + nota3) / 3
+    return prom
 
-#Le daremos un poco de presentación y por ahora solo lo trabajaremos con un solo estudiante para el buen uso de la función
-#También le añadiremos el ingreso de datos a traves de la consola
-print("Bienvenidos al programa que calcula promedios de estudiantes")
-
+#dejaré mos tanto las funciones como los objetos arriba.
 class Estudiante:
-    def __init__(self, id, nombre, apellido, nota1, nota2, nota3):
+    def __init__(self, id, nombre, apellido, nota1, nota2, nota3, promedio):	
         
         self.nombre = nombre
         self.apellido = apellido
         self.nota1 = nota1
         self.nota2 = nota2
         self.nota3 = nota3
+        self.promedio = promedio
 
-nombre= input("Ingrese el nombre del estudiante: ")
-apellido= input("Ingrese el apellido del estudiante: ")
-nota1= float(input("Ingrese la primera nota del estudiante: "))
-nota2= float(input("Ingrese la segunda nota del estudiante: "))
-nota3= float(input("Ingrese la tercer nota del estudiante: "))
 
-estudiante = Estudiante(1, nombre, apellido, nota1, nota2, nota3)
+#Con nuestro objeto Estudiante podemos empezar a ingresar los datos de los estudiantes a través de un bucle
+#Para esto solicitaremos la cantidad de estudiantes en el curso
 
-promedio = Promedio(estudiante.nota1, estudiante.nota2, estudiante.nota3)
+print("Bienvenidos al programa que calcula promedios de estudiantes")
 
-print(f"El promedio del estudiante {nombre} {apellido} es: {promedio}")
+#ingresa la variable que contará la cantidad de estudiantes
+#ingresamos una lista vacia para la cantidad de alumnos
+cantidad = int(input("Ingrese la cantidad de estudiantes: "))
+estudiantes = []
 
-#Esto facilita el ingreso de datos pero solo nos deja guardar un solo estudiante por ahora.
-#Con el objeto Estudiante, será mucho más sencillo solucitar y almacenar datos.
-#Se establecen datos float con el fin de digitar valores decimales
+#con el bucle for, ingresaremos los datos de los estudiantes numerandolos 1 a 1 y añadiendolos a la lista
+for i in range(1, cantidad+1):
+    nombre= input(f"Ingrese el nombre del estudiante {i}: ")
+    apellido= input(f"Ingrese el apellido del estudiante {i}: ")
+    nota1= float(input(f"Ingrese la primera nota del estudiante {i}: "))
+    nota2= float(input(f"Ingrese la segunda nota del estudiante {i}: "))
+    nota3= float(input(f"Ingrese la tercer nota del estudiante {i}: "))
+    promedio = Promedio(nota1, nota2, nota3)
+    estudiante = Estudiante(i, nombre, apellido, nota1, nota2, nota3, promedio)
+    estudiantes.append(estudiante)
+
+
+print(f"El promedio del ultimo estudiante ingresado es {estudiante.nombre} {estudiante.apellido} es: {estudiante.promedio}")
+
+#Para imprimir los datos de los estudiantes es necesesario implementar otro bucle for
+#También pondrémos funciones validadores y para acortar el codigo usaremos otro bucle dentro del bucle for el cual se encarge de validar los datos de los estudiantes
